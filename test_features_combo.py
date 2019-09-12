9a1806b54be7e822389dcbab2b58d671fb8f53b6
William Budington <bill@eff.org>
2019-09-12 11:39:30 -0700

    Adding helper script to test feature combos

---
+++ test_features_combo.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+
+def powerset(input):
+    if len(input) == 0:
+        return [[]]
+
+    pivot = input[0]
+
+    subset = powerset(input[1:])
+    with_pivot = subset.copy()
+    for i, set in enumerate(with_pivot):
+        with_pivot[i] = [pivot] + set
+
+    return subset + with_pivot
+
+
+def check(toolchain, features):
+    for subset in powerset(features):
+        feature_str = ",".join(subset)
+        print("$ cargo +" + toolchain + " check --no-default-features --features " + feature_str)
+
+        status = subprocess.run([
+            "cargo", "+" + toolchain, "check", "--no-default-features", "--features", feature_str
+        ])
+        if status.returncode != 0:
+            sys.exit(1)
+
+        print("$ cargo +" + toolchain + " test --no-default-features --no-run --features " + feature_str)
+
+        status = subprocess.run([
+            "cargo", "+" + toolchain, "test", "--no-default-features", "--no-run", "--features", feature_str
+        ])
+        if status.returncode != 0:
+            sys.exit(1)
+
+
+
+
+features = [
+    "rewriter",
+    "updater",
+    "add_rulesets",
+    "potentially_applicable",
+    "settings",
+]
+
+check("stable", features)
