f7b336624fcbd9fee54c714da6b92cd754bd386c
William Budington <bill@eff.org>
2020-12-17 18:14:30 -0800

    Adding cfg-if for conditional compilation

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,44 @@
 pub mod rulesets;
 pub use rulesets::RuleSets;
 
-#[cfg(feature="settings")]
-pub mod settings;
-#[cfg(feature="settings")]
-pub use settings::Settings;
+mod strings;
 
-#[cfg(feature="updater")]
-pub mod updater;
-#[cfg(feature="updater")]
-pub use updater::Updater;
+cfg_if::cfg_if! {
+    if #[cfg(feature="settings")] {
+        pub mod settings;
+        pub use settings::Settings;
+    }
+}
 
-#[cfg(feature="rewriter")]
-pub mod rewriter;
-#[cfg(feature="rewriter")]
-pub use rewriter::Rewriter;
+cfg_if::cfg_if! {
+    if #[cfg(feature="updater")] {
+        pub mod updater;
+        pub use updater::Updater;
+    }
+}
 
-#[cfg(feature="get_simple_rules_ending_with")]
-pub mod regex;
-#[cfg(feature="get_simple_rules_ending_with")]
-pub use crate::regex::RegEx;
+cfg_if::cfg_if! {
+    if #[cfg(feature="rewriter")] {
+        pub mod rewriter;
+        pub use rewriter::Rewriter;
+    }
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(feature="get_simple_rules_ending_with")] {
+        pub mod regex;
+        pub use crate::regex::RegEx;
+    }
+}
 
 #[cfg(any(feature="settings",feature="updater",feature="rewriter"))]
 mod storage;
-#[cfg(any(feature="settings",feature="rewriter",feature="updater"))]
+#[cfg(any(feature="settings",feature="updater",feature="rewriter"))]
 pub use storage::Storage;
 
 #[cfg(any(feature="rewriter",feature="updater"))]
 #[macro_use]
 extern crate log;
-
-mod strings;
 
 #[cfg(any(all(test,feature="add_rulesets"),feature="updater"))]
 #[macro_use]
